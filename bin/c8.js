c17e28ea2f5cfc9783d2169e9a472f89136d4798
Matteo Collina <hello@matteocollina.com>
2026-05-28 16:31:18 +0000

    fix: repair CI by adding --100 implementation, boolean normalization, and command dispatch

--- bin/c8.js
+++ bin/c8.js
@@ -10,12 +10,31 @@ const {
   hideInstrumenterArgs
 } = require('../lib/parse-args')
 
+const { checkCoverages } = require('../lib/commands/check-coverage')
+const Report = require('../lib/report')
 const instrumenterArgs = hideInstrumenteeArgs()
 let argv = parseArgs().parse(instrumenterArgs)
 
 async function run () {
-  if (['check-coverage', 'report'].indexOf(argv._[0]) !== -1) {
+  if (argv._[0] === 'check-coverage') {
     argv = parseArgs().parse(process.argv.slice(2))
+    const report = Report({
+      include: argv.include,
+      exclude: argv.exclude,
+      extension: argv.extension,
+      reporter: Array.isArray(argv.reporter) ? argv.reporter : [argv.reporter],
+      reportsDirectory: argv['reports-dir'],
+      tempDirectory: argv['temp-directory'],
+      watermarks: argv.watermarks,
+      resolve: argv.resolve,
+      omitRelative: argv['omit-relative'],
+      wrapperLength: argv['wrapper-length'],
+      all: argv.all
+    })
+    await checkCoverages(argv, report)
+  } else if (argv._[0] === 'report') {
+    argv = parseArgs().parse(process.argv.slice(2))
+    await outputReport(argv)
   } else {
     if (argv.clean) {
       await rm(argv['temp-directory'], { recursive: true, force: true })
